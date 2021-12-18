b5b5473388384d3912f9fc4eabfd3a36005279bf
Eduardo Dahmer Correa <eduardodahmer99@gmail.com>
2021-12-18 13:47:27 -0300

    Iniciando o projeto e adicionando novas features como Descontos e calculo de descontos

---
+++ src/CalculadoraDeDescontos.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+use Alura\DesignPattern\Descontos\Desconto;
+use Alura\DesignPattern\Descontos\DescontoMaisDe500Reais;
+use Alura\DesignPattern\Descontos\DescontoMaisDe5Itens;
+use Alura\DesignPattern\Descontos\SemDesconto;
+
+class CalculadoraDeDescontos
+{
+    public function CalculaDescontos(Orcamento $orcamento): float
+    {
+        $cadeiaDescontos = new DescontoMaisDe5Itens(
+            new DescontoMaisDe500Reais(
+                new SemDesconto()
+            )
+        );
+
+        return $cadeiaDescontos->calculaDesconto($orcamento);
+     }
+}
---
+++ testes.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Alura\DesignPattern\CalculadoraDeDescontos;
+use Alura\DesignPattern\CalculadoraDeImpostos;
+use Alura\DesignPattern\Impostos\ICMS;
+use Alura\DesignPattern\Orcamento;
+
+/*$calculadora = new CalculadoraDeImpostos();
+
+$orcamento = new Orcamento();
+$orcamento->valor = 100;
+
+echo $calculadora->calcula($orcamento, new ICMS());*/
+
+$calculadora = new CalculadoraDeDescontos();
+$orcamento = new Orcamento();
+$orcamento->valor = 600;
+$orcamento->quantidadeItens = 6;
+
+echo $calculadora->CalculaDescontos($orcamento);
