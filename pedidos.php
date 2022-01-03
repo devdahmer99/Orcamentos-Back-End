0b54e6618be7ef5a70d2b0e812d671a17048a442
eduardo <eduardodahmer99@gmail.com>
2022-01-03 20:53:58 -0300

    Implementação do padrão estrutural Flyweight.

---
+++ pedidos.php
@@ -0,0 +1,22 @@
+<?php
+
+use Alura\DesignPattern\DadosExtrinsecosPedido;
+use Alura\DesignPattern\Orcamento;
+use Alura\DesignPattern\Pedido;
+
+require_once 'vendor/autoload.php';
+
+$pedidos = [];
+$dados = new DadosExtrinsecosPedido();
+$dados->dataFinalizacao = new DateTimeImmutable();
+$dados->nomeCliente = md5('a');
+
+for ($i = 0; $i < 10000; $i++) {
+    $pedido = new Pedido();
+    $pedido->dados = $dados;
+    $pedido->orcamento = new Orcamento();
+
+    $pedidos[] = $pedido;
+}
+
+echo memory_get_peak_usage();
